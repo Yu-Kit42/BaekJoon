a114481940ea52ffa239f7e4a3ca6001711affec
SongJuHwan <93521202+Yu-Kit42@users.noreply.github.com>
2022-09-02 17:15:06 +0900

    [Bronze III] Title: 주사위 게임, Time: 320 ms, Memory: 23352 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.java
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.java
@@ -1,36 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[3];
-        String[] temp;
-        int out = 0;
-        int max = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int N[] = new int[input];
 
-        for (int i = 0; i < N; i++){
-            out = 0;
-            temp = bf.readLine().split(" ");
-            arr[0] = Integer.parseInt(temp[0]);
-            arr[1] = Integer.parseInt(temp[1]);
-            arr[2] = Integer.parseInt(temp[2]);
+        for (int i = 0; i < input; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
 
-            if ((arr[0] == arr[1]) && (arr[0] == arr[2])){
-                out += arr[0] * 1000 + 10000;
-            } else if (arr[0] == arr[1] || arr[0] == arr[2]) {
-                out += arr[0] * 100 + 1000;
-            } else if (arr[1] == arr[2]) {
-                out += arr[1] * 100 + 1000;
-            } else {
-                out += Math.max(Math.max(arr[0], arr[1]), arr[2]) * 100;
+            if (a == b && a == c && b == c)
+                N[i] = 10000 + a * 1000;
+            else if (a == b || a == c)
+                N[i] = 1000 + a * 100;
+            else if (b == c)
+                N[i] = 1000 + b * 100;
+            else {
+                int max = Math.max(a, Math.max(b, c));
+                N[i] = max * 100;
             }
-            if (max < out) max = out;
         }
-        System.out.println(max);
-
-
+        sc.close();
+        Arrays.sort(N);
+        System.out.print(N[N.length - 1] + " ");
     }
 }
+
