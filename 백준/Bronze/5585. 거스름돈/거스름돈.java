3000903f3db5f941a183bb9fe7c60e657cef7ad4
SongJuHwan <93521202+Yu-Kit42@users.noreply.github.com>
2022-12-22 21:10:01 +0900

    [Bronze II] Title: 거스름돈, Time: 124 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = 1000 - Integer.parseInt(bf.readLine());
+        int count = 0;
+
+        count += N / 500;
+        N %= 500;
+        count += N / 100;
+        N %= 100;
+        count += N / 50;
+        N %= 50;
+        count += N / 10;
+        N %= 10;
+        count += N / 5;
+        N %= 5;
+        count += N / 1;
+        N %= 1;
+
+        System.out.println(count);
+    }
+}
